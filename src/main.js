1281f9d4bf7573714a257e2fbf723b1f6e310ca7
MIKISugiyama <howdy8355@gmail.com>
2026-08-11 15:56:38 +0900

    スタイル調整、フッターのスタイル追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import './scss/style.scss';
 import { initHeroGlow } from './js/heroGlow.js';
+import { copyEmail } from './js/copyEmail.js';
+
 
 document.addEventListener('DOMContentLoaded', () => {
+  // 挙動制御
+  copyEmail();
+  
   // 演出・アニメーション系
   initHeroGlow();
 });
-
